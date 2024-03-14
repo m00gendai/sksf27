bba3942459088b1c6e4f7ec6766e1b4ee24904d7
m00gendai <mrweber@gmx.ch>
2024-03-14 17:04:25 +0100

    Added NAvbar component Amended import paths

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import Navbar from "@/components/Navbar/Navbar";
+import "@/globals/globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "SKSF27",
@@ -15,8 +14,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="de">
+      <body>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
