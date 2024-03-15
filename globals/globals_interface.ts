647e72072f91fe61306c704845e5853c810a9f2f
m00gendai <mrweber@gmx.ch>
2024-03-15 14:42:03 +0100

    Global Interface file created

---
+++ globals/globals_interface.ts
@@ -0,0 +1,18 @@
+export interface Medium {
+    path: string
+    title: string
+    mime: string
+    type: string
+    description: string
+    tags: string[]
+    size: number
+    colors: string[]
+    width: number
+    height: number
+    _hash: string
+    _created: number
+    _modified: number
+    _cby: string
+    folder: string
+    _id: string
+  }
