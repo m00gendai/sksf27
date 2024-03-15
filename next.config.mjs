01bc3a9780645657bc53e746fbb8639d9d4419a3
m00gendai <mrweber@gmx.ch>
2024-03-15 14:41:38 +0100

    Image and i18n configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'cms.sksf27.ch',
+            port: '',
+            pathname: '/storage/uploads/**',
+          },
+        ],
+      },
+      i18n: {
+        locales: ["de_CH"],
+        defaultLocale: "de_CH",
+      }
+};
 
 export default nextConfig;
