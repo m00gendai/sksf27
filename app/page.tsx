bbaee5c439970668cd0b208e2336ccbeba307ca9
m00gendai <mrweber@gmx.ch>
2024-03-15 15:58:19 +0100

    Added QuickLinks component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Hero from "@/components/Hero/Hero";
+import QuickLinks from "@/components/QuickLinks/QuickLinks";
 
 export default function Home() {
   return (
     <main>
      <Hero />
+     <QuickLinks />
      <section>text</section>
     </main>
   );
