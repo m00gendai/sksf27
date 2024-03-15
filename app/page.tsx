2e2728f935d26473fba63cb38c410c1d2314b740
m00gendai <mrweber@gmx.ch>
2024-03-15 14:45:12 +0100

    Restructured layout Main is fullwidth, section will be maxWidth Added Hero component Removed unused imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,10 @@
-import SH from "@/public/SH.png"
-import Image from "next/image"
-import s from "@/app/Home.module.css"
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <main>
-      <div className={s.image}>
-      <Image 
-        src={SH}
-        fill={true}
-        alt={"Wappen Schaffhausen"}
-        style={{objectFit: "contain"}}
-      />
-      </div>
-      <div className={s.hero}>
-     <h1 className={s.title} >Schaffhauser Kantonalschützenfest 2027</h1>
-     <h2 className={s.date}>August 2027</h2>
-     </div>
+     <Hero />
+     <section>text</section>
     </main>
   );
 }
