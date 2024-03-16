03344edf29d34671aafe341502062a04972fb4c2
m00gendai <mrweber@gmx.ch>
2024-03-16 08:57:01 +0100

    Created Footer component

---
+++ components/Footer/Footer.tsx
@@ -0,0 +1,15 @@
+import s from "./Footer.module.css"
+
+export default async function Footer(){
+
+    const date:Date = new Date()
+    const currentYear:number = date.getFullYear()
+    const creationYear:number = 2024
+
+    return(
+        <footer className={s.footer}>
+            <div className={s.copyright}>{`© ${creationYear}${currentYear === creationYear ? "" : `-${currentYear}`} Schaffhauser Kantonalschützenfest 2027`}</div>
+            <div className={s.shamelessPlug} dangerouslySetInnerHTML={{__html: `Webdesign von <a href="https://mrweber.ch" target="_blank">mrweber.ch</a>`}}></div>
+        </footer>
+    )
+    }
