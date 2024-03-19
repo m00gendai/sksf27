6228d7e565bd8628c34767a5106e4235405f18a4
m00gendai <mrweber@gmx.ch>
2024-03-19 16:57:36 +0100

    Added Mobile Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Hero from "@/components/Hero/Hero";
+import Hero_Mobile from "@/components/Hero/Hero_Mobile";
 import MapContainer from "@/components/Map/MapContainer";
 import QuickLinks from "@/components/QuickLinks/QuickLinks";
+import "@/globals/globals.css";
 
 export default function Home() {
   return (
     <main>
      <Hero />
+     <Hero_Mobile />
      <QuickLinks />
      <MapContainer />
      <section></section>
