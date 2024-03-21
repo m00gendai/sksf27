cc1857f8471663038c3e04ce1a51d6263a59701d
m00gendai <mrweber@gmx.ch>
2024-03-21 14:11:57 +0100

    Generating elements dynamically

--- components/Organisation/Organisation.tsx
+++ components/Organisation/Organisation.tsx
@@ -9,112 +9,36 @@ interface Props{
 
 export default function Organisation({people}:Props){
 
+    const categories: string[] = Array.from(new Set(people.map(person=>person.category)))
+
     return(
         <>
         <h2>Personen im Detail</h2>
-        <div className={s.container}>
-            <h3 >Präsidium</h3>
-            <div className={s.cardContainer}>    
-            {people.map(person =>{
-                if(person.category === "Präsidium"){
-                    return(
-                        <div id={`${person.title}`} className={s.card} key={`card_${person._id}`}>
-                            <div className={s.image}>
-                                    <Image src={person.foto ? `${process.env.NEXT_PUBLIC_STORAGE}${person.foto.path}` : TestBild.src} alt="" fill={true} style={{objectFit: "contain"}}/>
+        {categories.map(category =>{
+            return(
+                <div className={s.container}>
+                    <h3>{category}</h3>
+                    <div className={s.cardContainer}>    
+                    {people.map(person =>{
+                        if(person.category === category){
+                            return(
+                                <div id={`${person.title}`} className={s.card} key={`card_${person._id}`}>
+                                    <div className={s.image}>
+                                            <Image src={person.foto ? `${process.env.NEXT_PUBLIC_STORAGE}${person.foto.path}` : TestBild.src} alt="" fill={true} style={{objectFit: "contain"}}/>
+                                        </div>
+                                    <div className={s.info}>
+                                        
+                                        <p className={s.title}>{person.title}</p>
+                                        <p className={s.name}>{person.name}</p>
+                                    </div>
                                 </div>
-                            <div className={s.info}>
-                                
-                                <p className={s.title}>{person.title}</p>
-                                <p className={s.name}>{person.name}</p>
-                            </div>
-                        </div>
-                    )
-                }
-            })}
-            </div>
-
-            <h3 >Schiesskomitee</h3>
-            <div className={s.cardContainer}>    
-            {people.map(person =>{
-                if(person.category === "Schiesskomitee"){
-                    return(
-                        <div id={`${person.title}`} className={s.card} key={`card_${person._id}`}>
-                            <div className={s.image}>
-                                    <Image src={person.foto ? `${process.env.NEXT_PUBLIC_STORAGE}${person.foto.path}` : TestBild.src} alt="" fill={true} style={{objectFit: "contain"}}/>
-                                </div>
-                            <div className={s.info}>
-                                
-                                <p className={s.title}>{person.title}</p>
-                                <p className={s.name}>{person.name}</p>
-                            </div>
-                        </div>
-                    )
-                }
-            })}
-            </div>
-
-            <h3 >Finanzen</h3>
-            <div className={s.cardContainer}>    
-            {people.map(person =>{
-                if(person.category === "Finanzen"){
-                    return(
-                        <div id={`${person.title}`} className={s.card} key={`card_${person._id}`}>
-                            <div className={s.image}>
-                                    <Image src={person.foto ? `${process.env.NEXT_PUBLIC_STORAGE}${person.foto.path}` : TestBild.src} alt="" fill={true} style={{objectFit: "contain"}}/>
-                                </div>
-                            <div className={s.info}>
-                                
-                                <p className={s.title}>{person.title}</p>
-                                <p className={s.name}>{person.name}</p>
-                            </div>
-                        </div>
-                    )
-                }
-            })}
-            </div>
-
-            <h3 >Logistik</h3>
-            <div className={s.cardContainer}>    
-            {people.map(person =>{
-                if(person.category === "Logistik"){
-                    return(
-                        <div id={`${person.title}`} className={s.card} key={`card_${person._id}`}>
-                            <div className={s.image}>
-                                    <Image src={person.foto ? `${process.env.NEXT_PUBLIC_STORAGE}${person.foto.path}` : TestBild.src} alt="" fill={true} style={{objectFit: "contain"}}/>
-                                </div>
-                            <div className={s.info}>
-                                
-                                <p className={s.title}>{person.title}</p>
-                                <p className={s.name}>{person.name}</p>
-                            </div>
-                        </div>
-                    )
-                }
-            })}
-            </div>
-
-            <h3 >Marketing & Medien</h3>
-            <div className={s.cardContainer}>    
-            {people.map(person =>{
-                if(person.category === "Marketing & Medien"){
-                    return(
-                        <div id={`${person.title}`} className={s.card} key={`card_${person._id}`}>
-                            <div className={s.image}>
-                                    <Image src={person.foto ? `${process.env.NEXT_PUBLIC_STORAGE}${person.foto.path}` : TestBild.src} alt="" fill={true} style={{objectFit: "contain"}}/>
-                                </div>
-                            <div className={s.info}>
-                                
-                                <p className={s.title}>{person.title}</p>
-                                <p className={s.name}>{person.name}</p>
-                            </div>
-                        </div>
-                    )
-                }
-            })}
-            </div>
-            
-             
-          </div>
+                            )
+                        }
+                    })}
+                    </div>           
+                </div>
+            )
+          })}
           </>
     )
 }
