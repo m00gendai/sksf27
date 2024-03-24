b6a6bb448fc663231b6aea7634335d8b67ad8b31
m00gendai <mrweber@gmx.ch>
2024-03-24 19:06:05 +0100

    Content for DSGVO page

--- app/datenschutz/page.tsx
+++ app/datenschutz/page.tsx
@@ -1,11 +1,40 @@
-import PageHeading from "@/components/PageHeading/PageHeading";
+import { PageContent } from "@/globals/globals_interface"
+import s from "../../components/PageHeading/PageHeading.module.css"
+
+async function getContent(){
+    const getContent:Response = await fetch(
+        'https://cms.sksf27.ch/api/content/items/content?filter=%7Bpage%3A%22Datenschutz%22%7D&populate=100',
+        {
+            headers: {
+                'api-key': `${process.env.CMS}`,
+            },
+            next: {
+                revalidate: 10,
+            }
+        }
+    )
+
+    const content:PageContent[] = await getContent.json()
+    return content[0]
+}
+
 
 export default async function Page(){
+
+    const pageContent:PageContent = await getContent()
+
     return(
         <main>
             <section>
-                <PageHeading />
-                <p>Diese Seite ist noch im Aufbau...</p>
+                <h1 className={s.heading}>{pageContent.page}</h1>
+                {pageContent.content.map(element =>{
+                    return(
+                        <>
+                        <h2>{element.title}</h2>
+                        <div className="content" dangerouslySetInnerHTML={{__html: element.text}}></div>
+                        </>
+                    )
+                })}
             </section>
         </main>
     )
