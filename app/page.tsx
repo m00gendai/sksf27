efcd9b99dc042d9d65e7c5eec5537e28b5b64244
m00gendai <mrweber@gmx.ch>
2024-03-24 19:35:13 +0100

    Added metadata generation

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,11 @@ import Hero_Mobile from "@/components/Hero/Hero_Mobile";
 import MapContainer from "@/components/Map/MapContainer";
 import QuickLinks from "@/components/QuickLinks/QuickLinks";
 import "@/globals/globals.css";
+import { pageMetadata } from "@/globals/utils";
+
+export async function generateMetadata(){
+  return pageMetadata("Home")
+}
 
 export default function Home() {
   return (
