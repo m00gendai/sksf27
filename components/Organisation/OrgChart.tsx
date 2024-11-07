0fa604e5b57b3bcf346ca30b633cd688384cc974
m00gendai <mrweber@gmx.ch>
2024-11-07 10:20:45 +0100

    Replaced org chart libary

--- components/Organisation/OrgChart.tsx
+++ components/Organisation/OrgChart.tsx
@@ -7,9 +7,11 @@ import TestBild from "@/public/testbild.png"
 import s from "./OrgChart.module.css"
 import { Suspense, useRef } from "react";
 import { Margin, Resolution, usePDF } from 'react-to-pdf';
+import { Tree, TreeNode } from 'react-organizational-chart';
 
 import React from "react";
 import { Button } from "@radix-ui/themes";
+import OrgPosition from "./OrgPosition";
 
 interface Props{
   org: OK[]
@@ -17,106 +19,6 @@ interface Props{
 
 
 export default function OrganizationalChart({org}:Props){
-
-  interface orgData{
-    title: string
-    titleClass: string
-    contentClass: string
-    add?: string
-    member: {name: string, add?: string, image_url: string}[]
-    children?: {title: string, titleClass: string, contentClass: string, member: {name: string, add?: string, image_url: string}[]}
-  }
-
-  const onClickNode = (data:orgData) => {
-   document.getElementById(`${data.title}`)?.scrollIntoView({behavior: "smooth", block: "center"})
-}
-
-
-/* Commence unholy generation of tree data */
-
-  const orgData = {
-    title: org[0].title,
-    member: [
-      {
-        name: org[0].name,
-        image_url: `${org[0].foto ? `${process.env.NEXT_PUBLIC_STORAGE}${org[0].foto.path}` : TestBild.src}`,
-      },
-    ],
-    children: org.map((person, index)=>{
-      if(person.parent === org[0].title){
-        return {
-          title: person.title,
-          member: [
-            {
-              name: person.name,
-              image_url: `${person.foto ? `${process.env.NEXT_PUBLIC_STORAGE}${person.foto.path}` : TestBild.src}`,
-            }
-          ],
-           children: org.map((sub, index)=>{ /* to avoid rendering a "0" if there are no children, the children array has first to be generated, then checked for length, and then if its not 0, generate it for real... its horrendous */
-            if(sub.parent === person.title){
-              return{
-                title: sub.title,
-                member: [
-                  {
-                    name: sub.name,
-                    image_url: `${sub.foto ? `${process.env.NEXT_PUBLIC_STORAGE}${sub.foto.path}` : TestBild.src}`,
-                  }
-                ]
-              }
-            } else return undefined
-          }).filter(obj => obj !== undefined).length !== 0 ? org.map((sub, index)=>{ /* here's the stupid zero-length-check */
-            if(sub.parent === person.title){
-              return{
-                title: sub.title,
-                member: [
-                  {
-                    name: sub.name,
-                    image_url: `${sub.foto ? `${process.env.NEXT_PUBLIC_STORAGE}${sub.foto.path}` : TestBild.src}`,
-                  }
-                ]
-              }
-            } else return undefined
-          }).filter(obj => obj !== undefined) : null
-        }
-      }
-    }).filter(obj => obj !== undefined)
-  };
-
-  const options = {
-    // default is `save`
-    method: 'open',
-    // default is Resolution.MEDIUM = 3, which should be enough, higher values
-    // increases the image quality but also the size of the PDF, so be careful
-    // using values higher than 10 when having multiple pages generated, it
-    // might cause the page to crash or hang.
-    resolution: Resolution.HIGH,
-    page: {
-       // margin is in MM, default is Margin.NONE = 0
-       margin: Margin.SMALL,
-       // default is 'A4'
-       format: 'letter',
-       // default is 'portrait'
-       orientation: 'landscape',
-    },
-    canvas: {
-       // default is 'image/jpeg' for better size performance
-       mimeType: 'image/png',
-       qualityRatio: 1
-    },
-    // Customize any value passed to the jsPDF instance and html2canvas
-    // function. You probably will not need this and things can break, 
-    // so use with caution.
-    overrides: {
-       // see https://artskydj.github.io/jsPDF/docs/jsPDF.html for more options
-       pdf: {
-          compress: true
-       },
-       // see https://html2canvas.hertzen.com/configuration for more options
-       canvas: {
-          useCORS: true
-       }
-    },
- };
 
  const { toPDF, targetRef } = usePDF(
   {filename: 'SKSF27 Organigramm.pdf', 
@@ -135,7 +37,29 @@ export default function OrganizationalChart({org}:Props){
     {/* On mobile, the org chart is not displayed. But having display: none; on it interferes with the PDF generation. It is thus handled with zero width/height in the .container CSS*/}
       <div className={`${s.container}`} > 
         <Suspense fallback={<p>Laden...</p>}>
-        <div ref={targetRef}><OrganizationChart  data={orgData} onClickNode={onClickNode}/> </div>
+        <div ref={targetRef}>
+          <Tree label={<OrgPosition data={org[0]} />}>
+            {
+              org.map((position, psnIndex) =>{
+                if(position.parent === org[0].title){
+                  {
+                    return org.filter(sub => sub.parent === position.title).length === 0 ? <TreeNode key={`psn_${psnIndex}`} label={<OrgPosition data={position} />} />: 
+                      <TreeNode key={`psn_${psnIndex}`} label={<OrgPosition data={position} />}>
+                        {
+                          org.map((sub, subIndex) =>{
+                            if(sub.parent === position.title){
+                              return <TreeNode key={`sub_${subIndex}`} label={<OrgPosition data={sub} />} />
+                            } 
+                          })
+                        }
+                      </TreeNode>
+                  }
+                }
+              })
+            }
+
+          </Tree>
+          </div>
         </Suspense>
       </div>
       <Button className={`desktop button`} onClick={() => toPDF()}>{`Als PDF speichern`}</Button>
---
+++ components/Organisation/OrgPosition.tsx
@@ -0,0 +1,24 @@
+import { OK } from "./Interface_Organisation";
+import s from "./OrgPosition.module.css"
+import Image from "next/image"
+import Testbild from "@/public/testbild.png"
+
+export default function OrgPosition({ data }: { data: OK }){
+
+    function detail(){
+        document.getElementById(`${data.title}`)?.scrollIntoView({behavior: "smooth", block: "center"})
+    }
+    return(
+        <div className={s.container} onClick={()=>detail()} >
+            <h3 className={s.title}>{data.title}</h3>
+            <div className={s.nameContainer}>
+                <div className={s.name}>
+                    {data.name}
+                </div>
+                <div className={s.imageContainer}>
+                    {data.foto ? <Image src={`${process.env.NEXT_PUBLIC_STORAGE}${data.foto.path}`} alt={""} fill={true} style={{objectFit: "cover"}} className="boxShadow"/> : <Image src={Testbild} alt={"Testbild"} fill={true} style={{objectFit: "cover"}} className="boxShadow"/>}
+                </div>
+            </div>
+        </div>
+    )
+}
