7fb6b06573aeaa87e2bb6b13bc8e4b6fc16ef2b0
Marcel Weber <74364298+m00gendai@users.noreply.github.com>
2025-07-18 10:33:42 +0000

    Removed self-plug and made shorter

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -12,11 +12,8 @@ export default async function Footer(){
         <footer className={s.footer}>
             <div className={s.copyright}><LuCopyright style={{margin:"0 0.25rem 0 0"}}/> {`${creationYear}${currentYear === creationYear ? "" : `-${currentYear}`} Schaffhauser Kantonalschützenfest 2027`}</div>
             <div className={s.mail}><LuMail style={{margin: "0 0.25rem 0 0"}}/><a href="mailto:info@sksf27.ch">info@sksf27.ch</a></div>
-            <div className={s.links}>
-                <Link href="/impressum"><LuExternalLink style={{margin: "0 0.25rem 0 0"}}/> Impressum</Link>
-                <Link href="/datenschutz"><LuExternalLink style={{margin: "0 0.25rem 0 0"}}/>Datenschutz</Link>
-            </div>
-            <div className={s.shamelessPlug}><a href="https://mrweber.ch" target="_blank"><LuPalette style={{margin: "0 0.25rem 0 0"}} />Webdesign von <u>mrweber.ch</u></a></div>
+            <div className={s.link}><Link href="/impressum"><LuExternalLink style={{margin: "0 0.25rem 0 0"}}/> Impressum</Link></div>
+            <div className={s.link}><Link href="/datenschutz"><LuExternalLink style={{margin: "0 0.25rem 0 0"}}/>Datenschutz</Link></div>
         </footer>
     )
     }
