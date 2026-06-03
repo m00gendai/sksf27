ff7f32d69c839b7d834b2c5fff13361084582522
Marcel Weber <mrweber@gmx.ch>
2026-06-04 00:01:08 +0200

    Added DocumentGallery

--- components/PageContent/PageContent.tsx
+++ components/PageContent/PageContent.tsx
@@ -1,21 +1,27 @@
 import { SiteContent } from "@/globals/globals_interface"
 import Gallery from "../Gallery/Gallery"
+import DocumentGallery from "../DocumentGallery/DocumentGallery"
 
 interface Props{
     siteContent:SiteContent[]
 }
 
 export default function PageHeading({siteContent}:Props){
+    console.log(siteContent[0])
     return(
         <>
         {
             siteContent[0].content.map((section, index) =>{
+                const sectionDocuments = section.sectionAssets?.filter(asset => asset.type === "document")
+                const sectionImages = section.sectionAssets?.filter(asset => asset.mime.startsWith("image"))
                 return(
                     <div key={`${index}_${section.sectionTitle ? section.sectionTitle : "genericTitle"}`} className="content">
                         {section.sectionTitle ? <h3>{section.sectionTitle}</h3> : null}
                         <div className="content_text" dangerouslySetInnerHTML={{__html: section.sectionText}}></div>
-                        {section.sectionAssets ? <Gallery images={section.sectionAssets} /> : null}
+                        {sectionImages?.length > 0 ? <Gallery images={sectionImages} /> : null}  
+                        {sectionDocuments?.length > 0 ? <DocumentGallery documents={sectionDocuments} /> : null}
                     </div>
+                
                 )
             })
         }
